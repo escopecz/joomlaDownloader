528755982b873a92be393e4a18d673080aa5a02f
Jan Linhart <admin@escope.cz>
2013-12-24 16:53:08 +0100

    added redirect, new source

--- joomlaDownloader.php
+++ joomlaDownloader.php
@@ -53,6 +53,7 @@ class JoomlaDownloader
 		{
 	    		echo 'Caught exception: ',  $e->getMessage(), "\n";
 		}
+
 		echo 'Download errror';
 		return false;
 	}
@@ -60,20 +61,80 @@ class JoomlaDownloader
 	public function unzipFile()
 	{
 		$zip = new ZipArchive;
-		$res = $zip->open($this->localFile);
-
+		$res = $zip->open($this->filePath);
+		
 		if ($res === TRUE)
 		{
-		  $zip->extractTo($this->dirPath);
-		  $zip->close();
+			$extracted = $zip->extractTo($this->dirPath);
+			$zip->close();
 
-		  echo 'Joomla extracted!';
-		  return true;
+			if($extracted)
+			{
+				echo 'Joomla extracted!';
+				//$this->clean();
+				$this->redirect();
+				return true;
+			}
+			else
+			{
+				echo 'Zip extraction failed.'.$this->getZipError($res);
+				return false;
+			}
+			
 		}
 		else
 		{
-		  echo 'Extraction error!';
-		  return false;
+			echo 'Extraction error: '.$this->getZipError($res);
+			return false;
+		}
+	}
+
+	public function clean()
+	{
+		unlink(__FILE__);
+		unlink($this->localFile);
+	}
+
+	public function redirect()
+	{
+		echo '<script>window.location = "http://'.$_SERVER['HTTP_HOST'].'/installation/index.php";</script>';
+	}
+
+	public function getZipError($errorNumber)
+	{
+		$errors = array();
+		$errors[0] = 'No error';
+		$errors[1] = 'Multi-disk zip archives not supported';
+		$errors[2] = 'Renaming temporary file failed';
+		$errors[3] = 'Closing zip archive failed';
+		$errors[4] = 'S Seek error';
+		$errors[5] = 'Read error';
+		$errors[6] = 'Write error';
+		$errors[7] = 'CRC error';
+		$errors[8] = 'Containing zip archive was closed';
+		$errors[9] = 'No such file';
+		$errors[10] = 'File already exists';
+		$errors[11] = 'Can\'t open file';
+		$errors[12] = 'Failure to create temporary file';
+		$errors[13] = 'Zlib error';
+		$errors[14] = 'Malloc failure';
+		$errors[15] = 'Entry has been changed';
+		$errors[16] = 'Compression method not supported';
+		$errors[17] = 'Premature EOF';
+		$errors[18] = 'Invalid argument';
+		$errors[19] = 'Not a zip archive';
+		$errors[20] = 'Internal error';
+		$errors[21] = 'Zip archive inconsistent';
+		$errors[22] = 'Can\'t remove file';
+		$errors[23] = 'Entry has been deleted';
+
+		if(isset($errors[$errorNumber]))
+		{
+			return $errors[$errorNumber];
+		}
+		else
+		{
+			return 'Unknown error';
 		}
 	}
 
@@ -108,11 +169,11 @@ class JoomlaDownloader
 	    } 
 	    closedir($dir);
 	}
-
 }
 
 $localFile = 'joomla.zip';
-$joomlaZipUrl = 'https://github.com/joomla/joomla-cms/archive/master.zip';
+// $joomlaZipUrl = 'http://git.easysoftware.cz/webs/j2.5/easyjoomla/snapshot/easyjoomla-5dbddef5777209f4c5a57adac5d7114d97bc1144.zip';
+$joomlaZipUrl = 'http://joomlacode.org/gf/download/frsrelease/19007/134333/Joomla_3.2.1-Stable-Full_Package.zip';
 $downloader = new JoomlaDownloader($joomlaZipUrl, $localFile);
 
 
